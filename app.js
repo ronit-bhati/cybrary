58e96bbae96270a1efb1d807903c5983d0c70a1f
Ronit Bhati <ronitbhati.786@gmail.com>
2022-04-16 19:33:22 +0530

    added alerts and validation

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function Display() {
 
 };
 
-// adding methods to display's prototypes
+// methods to add books in display
 Display.prototype.add = function (book) {
     let tableBody = document.getElementById('tableBody');
     let uiHTML = `<tr>
@@ -21,15 +21,41 @@ Display.prototype.add = function (book) {
     tableBody.innerHTML += uiHTML;
 };
 
+// method to clear the form after book is added
 Display.prototype.clear = function () {
     let addBook = document.getElementById('addBook');
     addBook.reset();
+};
+
+// method to show alert according to type and messege you want
+Display.prototype.show = function (type, messege) {
+    let messegeHTML = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+                            <strong>Hey There!</strong> ${messege}
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>`;
+    let messegeLoc = document.getElementById('messege');
+    messegeLoc.innerHTML = messegeHTML;
+    setTimeout(() => {
+        messegeLoc.innerHTML = '';
+    }, 4000);
+};
+
+// method to validate that form is not invalid or empty
+Display.prototype.validate = function (newBook) {
+    if (newBook.name.length < 3 || newBook.author.length < 3) {
+        return false;
+    } else {
+        return true;
+    };
 };
 
 // adding event listener in form to add book
 let addBook = document.getElementById('addBook');
 addBook.addEventListener('submit', submitBook);
 
+// function to submit the book by form to display
 function submitBook(form) {
     form.preventDefault();
     let bName = document.getElementById('bookName').value;
@@ -52,8 +78,13 @@ function submitBook(form) {
     };
 
     let newBook = new book(bName, bAuthor, bType);
-
     let display = new Display();
-    display.add(newBook);
-    display.clear();
+
+    if (display.validate(newBook) == false) {
+        display.show('warning', 'Please fill all the fields correctly to continue.');
+    } else {
+        display.show('success', 'Your book has been added successfully.');
+        display.add(newBook);
+        display.clear();
+    };
 };
